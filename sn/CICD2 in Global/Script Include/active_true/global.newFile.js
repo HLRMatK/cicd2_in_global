384e7774c767242734d55627b348288646b79c8c
System Administrator <none>
2019-06-03 16:45:33 +0200

    Changes by 'System Administrator' on 'CICD2 in Global – 1.0.1' -- Build #1

---
+++ sn/CICD2 in Global/Script Include/active_true/global.newFile.js
@@ -0,0 +1,37 @@
+/* 
+ * Application : CICD2 in Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-06-03 14:26:41
+ * Created By  : admin
+ * Updated On  : 2019-06-03 14:27:42
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=4e077e79db3173001ac4a565ca961921
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof global.module:sys_script_include
+ */
+var newFile = Class.create();
+newFile.prototype = /** @lends global.module:sys_script_include.newFile.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+	
+	/**
+	 * Description
+	 * 
+	 * @returns {boolean} 
+	 */
+	testMe: function() {
+		return true;
+	},
+
+    type: 'newFile'
+};
