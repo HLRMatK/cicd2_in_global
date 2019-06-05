7eb9d4b9e0b3e2a4f517c0ca85a00cd4fbaa3f35
MatK <42271393+HLRMatK@users.noreply.github.com>
2019-06-05 10:49:49 +0200

    revert test
    revert

--- sn/CICD2 in Global/Script Include/active_true/global.newFile.js
+++ sn/CICD2 in Global/Script Include/active_true/global.newFile.js
@@ -3,7 +3,11 @@
  * ClassName   : sys_script_include
  * Created On  : 2019-06-03 14:26:41
  * Created By  : admin
+<<<<<<< HEAD
  * Updated On  : 2019-06-05 08:30:01
+=======
+ * Updated On  : 2019-06-03 14:52:06
+>>>>>>> parent of 2cbd9fe... CICD2 in Global – 1.0.1 (System Administrator) #1 (#6)
  * Updated By  : admin
  * URL         : /sys_script_include.do?sys_id=4e077e79db3173001ac4a565ca961921
  */
@@ -31,7 +35,11 @@ newFile.prototype = /** @lends global.module:sys_script_include.newFile.prototyp
 	 */
 	testMe: function() {
 		/* this is a new comment */
+<<<<<<< HEAD
 		var a = 13;
+=======
+		var a = 11;
+>>>>>>> parent of 2cbd9fe... CICD2 in Global – 1.0.1 (System Administrator) #1 (#6)
 		return a>0;
 	},
 
